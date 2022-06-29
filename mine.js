81f7653a8c21f1739bc8130f3daac15bcb4c704e
antiadamiani <102787453+antiadamiani@users.noreply.github.com>
2022-06-29 21:37:03 +0400

    added to do shopping list

--- mine.js
+++ mine.js
@@ -1,17 +1,32 @@
-let title = document.querySelector(".title");
-let inputText = document.querySelector(".inputText");
-let toDoContainer = document.querySelector(".toDoContainer");
+let input = document.querySelector(".input");
+let chamonatvali = document.querySelector(".chamonatvali");
 let buttonAdd = document.querySelector(".buttonAdd");
-buttonAdd.addEventListener("click", function (){
-   let paragraph = document.createElement("p");
-   paragraph.classList.add("paragraph_styling")
-   paragraph.innerHTML = inputText.value;       
-   toDoContainer.appendChild(paragraph);
-   inputText.value = "";
-   paragraph.addEventListener("click", function(){
-       paragraph.style.textDecoration = "line_through"
-   })
-   paragraph.addEventListener("dblclick", function(){
-    paragraph.parentNode.removeChild(paragraph);
+let contentCounter = 0;
+buttonAdd.addEventListener("click", function () {
+   contentCounter++;
+
+   buttonAdd.addEventListener("click", function () {
+      if (input.value == "") {
+         console.log("input ცარიელია");
+      } else {
+         let content = document.createElement("div");
+         content.classList.add(`content${contentCounter}`);
+         content.classList.add("content");
+         chamonatvali.appendChild(content);
+         let paragraph = document.createElement("p");
+         paragraph.classList.add("paragraph_styling");
+         paragraph.innerHTML = input.value;
+         input.value = "";
+         content.appendChild(paragraph);
+         let krestik = document.createElement("img");
+         krestik.src = "https://t4.ftcdn.net/jpg/03/46/38/39/360_F_346383913_JQecl2DhpHy2YakDz1t3h0Tk3Ov8hikq.jpg"
+         krestik.classList.add("krestik");
+         content.appendChild(krestik);
+         krestik.addEventListener("click", function () {
+            content.remove(content);
+         })
+
+      }
    })
 })
+
